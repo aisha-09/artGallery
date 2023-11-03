7087f1da2ae40f731965d859cfdca034cb12b2e0
Aisha-09 <aisha92002@gmail.com>
2023-11-03 10:47:24 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BrowserComponent } from './browser/browser.component';
+import { SearchComponent } from './search/search.component';
+import { DetailsComponent } from './details/details.component';
+import { CollectionsComponent } from './collections/collections.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/browse', pathMatch:'full'},
+  {path: 'browse', component: BrowserComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'details', component: DetailsComponent},
+  {path: 'collections', component:CollectionsComponent},
+  {path: 'favorites', component:FavouritesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/favourites/favourites.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ArtService } from '../shared/art.service';
+import { ArtworkdetailComponent } from '../artworkdetail/artworkdetail.component';
+import { MatDialog } from '@angular/material/dialog';
+
+
+
+
+
+@Component({
+  selector: 'app-favourites',
+  templateUrl: './favourites.component.html',
+  styleUrls: ['./favourites.component.css']
+})
+export class FavouritesComponent {
+  favorites: any[] = []; // modify favorites type to any[]
+  notification: string | null = null;
+
+  constructor( public dialog: MatDialog
+    ) { }
+
+  ngOnInit(): void {
+    const savedFavorites = localStorage.getItem('favorites');
+    if (savedFavorites) {
+      this.favorites = JSON.parse(savedFavorites);
+    }
+  }
+
+  openDetail(art:any): void {
+    this.dialog.open(ArtworkdetailComponent,{
+      width: '600px',
+      data: art
+    });
+  }
+  getArtworkImageUrl(imageId: string): string {
+    return `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
+  }
+  
+
+
+addToFavorites(artwork: any): void {
+  const exists = this.favorites.some(fav => fav.image_id === artwork.image_id);
+
+  if (!exists) {
+    this.favorites.push(artwork);
+    localStorage.setItem('favorites', JSON.stringify(this.favorites));
+    this.notification = 'Artwork added to favorites!';
+  } else {
+    this.notification = 'This artwork is already in your favorites!';
+  }
+}
+
+
+removeFromFavorites(artwork: any): void {
+  const index = this.favorites.findIndex(fav => fav.image_id === artwork.image_id);
+
+  if (index > -1) {
+    this.favorites.splice(index, 1);
+    localStorage.setItem('favorites', JSON.stringify(this.favorites));
+    this.notification = 'Artwork removed from favorites!';
+  }
+}
+
+dismissNotification(): void {
+  this.notification = null;
+}
+
+  
+
+}
